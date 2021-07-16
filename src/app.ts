50aee3a0761a29eb4e775b0cf4090e05ffebeca6
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2021-07-16 13:31:56 -0400

    use cors and morgan

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(morgan('dev'));
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
 
 export default app;
