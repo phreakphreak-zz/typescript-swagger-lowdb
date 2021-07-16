cd8eb2ab108fad4809b2c6dc8a40defd4e4083be
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2021-07-16 18:57:15 -0400

    server with http module

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,71 @@
-import express, { Request, Response } from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-const app = express();
+import {
+  Server,
+  createServer,
+  RequestListener,
+  IncomingMessage,
+  ServerResponse
+} from 'http';
 
-// Middlewares
-app.use(cors());
-app.use(morgan('dev'));
+interface IRoute {
+  path: string;
+  status: number;
+  content: string;
+}
 
-// Routes
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+const Routes: IRoute[] = [
+  {
+    path: '/',
+    status: 200,
+    content: '<h1>Hello World!</h1>'
+  },
+  {
+    path: '/about',
+    status: 200,
+    content: '<h1>About</h1>'
+  },
+  {
+    path: '/contact',
+    status: 200,
+    content: '<h1>Contact</h1>'
+  }
+];
+
+const handleRoutes = (path: string | undefined): Promise<IRoute> => {
+  return new Promise((resolve, reject) => {
+    try {
+      if (!path) throw new Error('Invalid path');
+      const route = Routes.find((route) => route.path === path);
+      if (!route) throw new Error('Route not found');
+      resolve(route);
+    } catch (error) {
+      console.log(error.message);
+      reject({
+        path: null,
+        status: 404,
+        content: '<h1>404 Not Found</h1>'
+      });
+    }
+  });
+};
+
+const handleResponse = (res: ServerResponse, route: IRoute) => {
+  res.writeHead(route.status, { 'Content-Type': 'text/html' });
+  res.end(route.content);
+};
+
+const requestListener: RequestListener = async (
+  req: IncomingMessage,
+  res: ServerResponse
+) => {
+  try {
+    const path: string | undefined = req.url?.split('?')[0].toLowerCase();
+    const route: IRoute = await handleRoutes(path);
+    handleResponse(res, route);
+  } catch (error) {
+    handleResponse(res, error);
+  }
+};
+
+const app: Server = createServer(requestListener);
 
 export default app;
