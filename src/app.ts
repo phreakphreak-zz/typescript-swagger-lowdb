164176569a035ae7c5d66352b7c6e951da93bc5e
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2021-07-17 00:51:27 -0400

    add static file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
-import routes from '../routes';
+import routes from './routes';
+import path from 'path';
 
 const app = express();
+
+// static files
+app.use(express.static(path.join(path.resolve('./'), 'public')));
 
 // settings
 app.set('port', process.env.PORT || 3000);
